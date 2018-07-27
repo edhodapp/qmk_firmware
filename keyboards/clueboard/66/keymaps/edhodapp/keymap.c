84d2853225d43adf0941505c4e6ac72bc4efbe44
Edward Hodapp <ed@Edwards-MacBook-Air.local>
2018-07-26 21:39:56 -0700

    Started using control layer.

--- keyboards/clueboard/66/keymaps/edhodapp/keymap.c
+++ keyboards/clueboard/66/keymaps/edhodapp/keymap.c
@@ -14,23 +14,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,                KC_END,
   KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, KC_ENT,
   KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RO,  KC_RSFT,        KC_UP,
-  KC_LALT,MO(_FL),KC_LGUI,_______,        KC_SPC, KC_SPC,                         _______,KC_RGUI,MO(_FL),KC_CAPS,KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LALT,MO(_FL),KC_LGUI,_______,        KC_SPC, KC_SPC,                         _______,KC_RGUI,MO(_FL),MO(_CL),KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
 [_FL] = LAYOUT(
   KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,        KC_VOLU,
   _______,_______,_______,_______,_______,_______,_______,_______,KC_INS, _______,KC_PSCR,_______,_______,KC_DEL,                 KC_VOLD,
-  _______,_______,MO(_CL),_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        KC_PGUP,
-  _______,_______,_______,_______,        _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
+  _______,MO(_FL),_______,_______,        _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
 
   /* Keymap _CL: Control layer
    */
 [_CL] = LAYOUT(
   BL_STEP,RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_SW,RGB_M_SN,RGB_M_K,RGB_M_X,RGB_M_G,_______,_______,_______,_______,_______,RGB_TOG,        RGB_VAI,
   _______,_______,_______,_______,RESET,  _______,_______,_______,_______,_______,_______,_______,_______,_______,                RGB_VAD,
-  _______,_______,MO(_CL),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-  MO(_FL),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        RGB_SAI,
-  _______,_______,_______,_______,        RGB_MOD,   RGB_MOD,                     _______,_______,MO(_FL),_______,RGB_HUD,RGB_SAD,RGB_HUI),
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        RGB_SAI,
+  _______,_______,_______,_______,        RGB_MOD,   RGB_MOD,                     _______,_______,_______,MO(_CL),RGB_HUD,RGB_SAD,RGB_HUI),
 };
